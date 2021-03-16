342d462e903686729744d1096348f64a1e9ece14
Samuel De Backer <sdebacker@gmail.com>
2021-03-16 22:57:14 +0100

    Update TermFormRequest.php

--- src/Http/Requests/TermFormRequest.php
+++ src/Http/Requests/TermFormRequest.php
@@ -9,7 +9,6 @@ class TermFormRequest extends AbstractFormRequest
     public function rules()
     {
         return [
-            'taxonomy_id' => 'required|integer',
             'title.*' => 'nullable|max:255',
             'slug.*' => 'nullable|alpha_dash|max:255|required_with:title.*',
         ];
