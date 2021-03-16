700a8daa7c1030359ad052c31b88951f03364e08
Samuel De Backer <sdebacker@gmail.com>
2021-03-16 22:18:11 +0100

    Create _checkboxes.blade.php

---
+++ resources/admin/_checkboxes.blade.php
@@ -0,0 +1,13 @@
+@if ($taxonomies = Taxonomies::whereRaw('JSON_CONTAINS(modules, \'"'.$module.'"\')')->order()->get() and $taxonomies->count() > 0)
+    @foreach ($taxonomies as $taxonomy)
+    <div class="col-md-3">
+        <label class="form-label" for="">{{ $taxonomy->title }}</label>
+        @foreach ($taxonomy->terms as $term)
+        <div class="form-check">
+            {!! Form::checkbox('terms[]', $term->id)->id('term_'.$term->id)->addClass('form-check-input') !!}
+            <label class="form-check-label" for="{{ 'term_'.$term->id }}">{{ $term->title }}</label>
+        </div>
+        @endforeach
+    </div>
+    @endforeach
+@endif
